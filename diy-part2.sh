44cbd74c4d3f706250a793a4d2e01f5615ccf55a
levenwindy <winyiday@qq.com>
2023-04-12 20:36:01 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,14 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.1.210/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.1.210/g' package/base-files/files/bin/config_generate
+
+# 设定ip dns
+sed -i "/exit 0/i\uci set network.lan.ipaddr='192.168.1.210'" package/lean/default-settings/files/zzz-default-settings 
+# sed -i "uci set network.lan.proto='static'" package/base-files/files/bin/config_generate
+# sed -i "uci set network.lan.type='bridge'" package/base-files/files/bin/config_generate
+# sed -i "uci set network.lan.ifname='eth0'" package/base-files/files/bin/config_generate
+# sed -i "uci set network.lan.netmask='255.255.255.0'" package/base-files/files/bin/config_generate
+sed -i "/exit 0/i\uci set network.lan.gateway='192.168.1.1'" package/lean/default-settings/files/zzz-default-settings 
+sed -i "/exit 0/i\uci set network.lan.dns='192.168.1.1'" package/lean/default-settings/files/zzz-default-settings 
+sed -i "/exit 0/i\uci commit network" package/lean/default-settings/files/zzz-default-settings 
