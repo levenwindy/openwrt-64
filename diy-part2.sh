cfe6f1cca83f7f369c338350270f644f277953bf
levenwindy <winyiday@qq.com>
2023-04-13 19:22:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,18 +11,19 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.1.210/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.210/g' package/base-files/files/bin/config_generate
 
 # 设定ip dns
-sed -i "/exit 0/i\uci set network.lan.ipaddr='192.168.1.210'" package/lean/default-settings/files/zzz-default-settings 
+# sed -i "/exit 0/i\uci set network.lan.ipaddr='192.168.1.210'" package/lean/default-settings/files/zzz-default-settings 
 # sed -i "uci set network.lan.proto='static'" package/base-files/files/bin/config_generate
 # sed -i "uci set network.lan.type='bridge'" package/base-files/files/bin/config_generate
 # sed -i "uci set network.lan.ifname='eth0'" package/base-files/files/bin/config_generate
 # sed -i "uci set network.lan.netmask='255.255.255.0'" package/base-files/files/bin/config_generate
-sed -i "/exit 0/i\uci set network.lan.gateway='192.168.1.1'" package/lean/default-settings/files/zzz-default-settings 
-sed -i "/exit 0/i\uci set network.lan.dns='192.168.1.1'" package/lean/default-settings/files/zzz-default-settings 
-sed -i "/exit 0/i\uci commit network" package/lean/default-settings/files/zzz-default-settings 
-cat package/lean/default-settings/files/zzz-default-settings 
+
+# sed -i "/exit 0/i\uci set network.lan.gateway='192.168.1.1'" package/lean/default-settings/files/zzz-default-settings 
+# sed -i "/exit 0/i\uci set network.lan.dns='192.168.1.1'" package/lean/default-settings/files/zzz-default-settings 
+# sed -i "/exit 0/i\uci commit network" package/lean/default-settings/files/zzz-default-settings 
+# cat package/lean/default-settings/files/zzz-default-settings 
 
 #- name: 替换默认主题 luci-theme-argon
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/' feeds/luci/collections/luci/Makefile
