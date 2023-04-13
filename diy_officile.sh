d037a3551e75850fb3b2bcc59c0f62c47eed3856
levenwindy <winyiday@qq.com>
2023-04-14 05:55:37 +0800

    Update diy_officile.sh

--- diy_officile.sh
+++ diy_officile.sh
@@ -11,17 +11,17 @@
 #
 
 # Modify default IP
-# sed -i 's/192.168.1.1/192.168.1.210/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.210/g' package/base-files/files/bin/config_generate
 
 # 设定ip dns
-sed -i "/exit 0/i\uci set network.lan.ipaddr='192.168.1.210'" feeds/small8/.github/diy/zzz-default-settings
+#sed -i "/exit 0/i\uci set network.lan.ipaddr='192.168.1.210'" feeds/small8/.github/diy/zzz-default-settings
 # sed -i "uci set network.lan.proto='static'" package/base-files/files/bin/config_generate
 # sed -i "uci set network.lan.type='bridge'" package/base-files/files/bin/config_generate
 # sed -i "uci set network.lan.ifname='eth0'" package/base-files/files/bin/config_generate
 # sed -i "uci set network.lan.netmask='255.255.255.0'" package/base-files/files/bin/config_generate
-sed -i "/exit 0/i\uci set network.lan.gateway='192.168.1.1'" feeds/small8/.github/diy/zzz-default-settings
-sed -i "/exit 0/i\uci set network.lan.dns='192.168.1.1'" feeds/small8/.github/diy/zzz-default-settings
-sed -i "/exit 0/i\uci commit network" feeds/small8/.github/diy/zzz-default-settings
+#sed -i "/exit 0/i\uci set network.lan.gateway='192.168.1.1'" feeds/small8/.github/diy/zzz-default-settings
+#sed -i "/exit 0/i\uci set network.lan.dns='192.168.1.1'" feeds/small8/.github/diy/zzz-default-settings
+#sed -i "/exit 0/i\uci commit network" feeds/small8/.github/diy/zzz-default-settings
 
 # 添加NueXini_Packages 源
 # mkdir -p package/other
