f1925f74e4a0510602fd7edd8f7bb5930b8f688b
levenwindy <winyiday@qq.com>
2023-05-09 22:42:37 +0800

    Create diy-360T7_glinet.sh

---
+++ bash/diy-360T7_glinet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 默认ip 192.168.1.1
+# sed -i 's/192.168.1.1/192.168.1.210/g' package/base-files/files/bin/config_generate
+
+# 修改时区 UTF-8
+sed -i 's/UTC/CST-8/g'  package/base-files/files/bin/config_generate
+
+# 修改主机名 OP
+sed -i 's/ImmortalWrt/OpenWrt/g'  package/base-files/files/bin/config_generate
+
+# 增加源
+echo 'src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
