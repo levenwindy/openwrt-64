35491d613b9361900abdbd8be4b0f86d8ac4b289
levenwindy <winyiday@qq.com>
2023-05-21 10:41:17 +0800

    Update diy-360T7_qihoo.sh

--- bash/diy-360T7_qihoo.sh
+++ bash/diy-360T7_qihoo.sh
@@ -32,8 +32,8 @@ sed -i 's/time1.google.com/ntp.aliyun.com/g'  package/base-files/files/bin/confi
 sed -i 's/time.cloudflare.com/cn.ntp.org.cn/g'  package/base-files/files/bin/config_generate
 sed -i 's/pool.ntp.org/cn.pool.ntp.org/g'  package/base-files/files/bin/config_generate
 
-# 替换腾讯 opkg 源
-sed -i 's/mirrors.pku.edu.cn/immortalwrt/mirrors.vsean.net/openwrt/g'  package/emortal/default-settings/files/99-default-settings-chinese
+# 替换腾讯 opkg 源 只有 mirrors.vsean.net 和 北大源
+# sed -i 's/mirrors.pku.edu.cn/immortalwrt/mirrors.vsean.net/openwrt/g'  package/emortal/default-settings/files/99-default-settings-chinese
 
 # 237176253 硬件QOS padavanonly/immortalwrtARM
 # sed -i "s/entry->ipv4_hnapt.winfo.wcid = skb_hnat_wc_id(skb);/entry->ipv4_hnapt.winfo.wcid = skb_hnat_wc_id(skb);\n			entry->ipv4_hnapt.iblk2.fqos = (IS_HQOS_MODE) ? 1 : 0;/g" target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat_nf_hook.c
